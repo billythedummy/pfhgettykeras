Fixed softmax bug

data_generator.py
@@ -7,21 +7,22 @@ import tensorflow as tf
 import keras
 import numpy as np
 import random
+import math
 # Image/Video Manipulation
 from PIL import Image
+from PIL import ImageEnhance
+from PIL import ImageFilter
 from resizeimage import resizeimage as ri
-from skimage.color import rgb2gray
 from scipy import ndimage
 import skimage as sk
 from skimage import util
-# Detect memory leaks
-from mem_top import mem_top
-import gc
+# Utility
+import matplotlib.pyplot as plt
 
 
 class DataGenerator(keras.utils.Sequence):
 
-    def __init__(self, x_train, y_train, batch_size=10, dim=(32, 32, 3), color_dict=None,shuffle=True):
+    def __init__(self, x_train, y_train, batch_size=10, dim=(32, 32, 3), color_dict=None, shuffle=True):
         'Initialization'
         # colour map
         self.color_dict = color_dict
@@ -45,26 +46,26 @@ class DataGenerator(keras.utils.Sequence):
             x, y = self.__data_generation(self.x_train, self.y_train, i)
             x_list.append(x)
             y_list.append(y)
-        return np.vstack(tuple(x_list)), np.vstack(tuple(y_list))
+        return np.stack(tuple(x_list)), np.stack(tuple(y_list))
 
     def on_epoch_end(self):
         # Updates indexes after each epoch
-        self.indexes = np.arange(len(self.x_train))
+        self.indexes = np.arange(len(self.y_train))
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
     def __data_generation(self, x_train, y_train, f):
-        rotation = random.uniform(-15, 15)
+        rotation = random.uniform(-30, 30)
         cur_frame_raw = x_train[f]
         cur_frame_raw = self.resize_image(
-            cur_frame_raw, self.dim[1], self.dim[0], rotation)
+            cur_frame_raw, self.dim[1], self.dim[0], rotation, enhance=True, resample=Image.LANCZOS)
 
         mask_frame = None
-        if f > 0:
+        if f > 0 and random.random() > 0.15:
             mask_frame = y_train[f-1]
             mask_frame = self.resize_image(
-                mask_frame, self.dim[1], self.dim[0], rotation, add_noise=False)
-            mask_frame = self.rgb2onehot(mask_frame)
+                mask_frame, self.dim[1], self.dim[0], rotation + random.uniform(-4, 4))
+            mask_frame = self.rgb2onehot_distorted(mask_frame)
         else:
             mask_frame = np.zeros(
                 shape=(self.dim[0], self.dim[1], len(self.color_dict)))
@@ -72,36 +73,48 @@ class DataGenerator(keras.utils.Sequence):
         # Add mask as another channel to current frame data to be passed into neural net
         cur_frame = np.dstack((cur_frame_raw, mask_frame))
 
-        # Keep a reference to current frame data with blank previous mask
-        cur_frame_raw = np.dstack(
-            (cur_frame_raw, np.zeros(shape=(self.dim[0], self.dim[1], len(self.color_dict)))))
-
-        # cur_frame = np.expand_dims(cur_frame, 0)
-        # cur_frame_raw = np.expand_dims(cur_frame_raw, 0)
-
         compare_frame = y_train[f]
         compare_frame = self.resize_image(
             compare_frame, self.dim[1], self.dim[0], rotation)
         compare_frame = self.rgb2onehot(compare_frame)
+        compare_frame = np.reshape(compare_frame, (self.dim[0] * self.dim[1], len(self.color_dict)))
         # compare_frame = np.expand_dims(compare_frame, 0)
-        return np.stack((cur_frame, cur_frame_raw)), np.stack((compare_frame, compare_frame))
+        return cur_frame, compare_frame
 
     # Resize/augment image, keeping all of image with black bars filling rest
-    def resize_image(self, im, width, height, rotation, fill_color=(0, 0, 0), add_noise=False):
+    def resize_image(self, im, width, height, rotation, fill_color=(0, 0, 0), enhance=False, resample=Image.NEAREST):
         im = Image.fromarray(im.astype('uint8'))
-        new_im = ri.resize_contain(im, (width, height), bg_color=fill_color)
+        new_im = ri.resize_contain(
+            im, (width, height), bg_color=fill_color, resample=resample)
 
-        new_im = new_im.rotate(rotation)
+        new_im = new_im.rotate(rotation, resample=Image.NEAREST)
+        if enhance:
+            # Image enhancements
+            new_im = ImageEnhance.Contrast(
+                new_im).enhance(random.uniform(0.7, 1.4))
+            new_im = ImageEnhance.Brightness(
+                new_im).enhance(random.uniform(0.7, 1.4))
+            new_im = ImageEnhance.Sharpness(
+                new_im).enhance(random.uniform(0.7, 1.4))
         im_array = np.asarray(new_im)
-        if(add_noise):
-            im_array = sk.util.random_noise(im_array, mode='s&p')
         return im_array
 
     def rgb2onehot(self, rgb_arr):
         num_classes = len(self.color_dict)
+        # shape will be original (width, height) with "num_classes" channels
         shape = rgb_arr.shape[:2]+(num_classes,)
-        arr = np.zeros(shape, dtype=np.int8)
+        arr = np.zeros(shape, dtype=np.float32)
+
+        # for each of our "num_classes" channels, each corresponding to a class...
         for i, cls in enumerate(self.color_dict):
-            arr[:, :, i] = np.all(rgb_arr.reshape(
-                (-1, 3)) == self.color_dict[i], axis=1).reshape(shape[:2])
+            all_pixels = rgb_arr.reshape((-1, 3))
+            matching_pixels = all_pixels == self.color_dict[i]
+            # assign back to this class's channel in the output
+            arr[:, :, i] = np.all(matching_pixels, axis=1).reshape(shape[:2])
         return arr
+
+    def rgb2onehot_distorted(self, rgb_arr):
+        noisiness = math.pow(random.uniform(0, 0.8), 2)
+        onehot = self.rgb2onehot(rgb_arr)
+        noise = np.random.rand(self.dim[0], self.dim[1], len(self.color_dict)) * noisiness
+        return np.abs(onehot - noise)

test.py
@@ -22,22 +22,26 @@ from model import SegmentationNetwork
 # from clr import CyclicLR
 IMAGE_INPUT_WIDTH = 512
 IMAGE_INPUT_HEIGHT = 256
-LOAD_MODEL_PATH = "./Models/PlateSegmentation/weights-03.hdf5"
+LOAD_MODEL_PATH = "./Models/PlateSegmentation/weights-25.hdf5"
 color_dict = {0: (0,   0, 0),  # 0: Background
-              1: (255, 255, 255),  # 1: Red
-              #   2: (0, 0, 255),  # 2: Blue
+              1: (255, 0, 0),  # 1: Red
+              2: (0, 0, 255),  # 2: Blue
               }
 model = SegmentationNetwork(
-    layers=4, start_filters=12, squeeze_factor=8,num_classes=len(color_dict)).create_model()
+    layers=4, start_filters=16, squeeze_factor=16, num_classes=len(color_dict), 
+    shape=(IMAGE_INPUT_HEIGHT, IMAGE_INPUT_WIDTH, 3)).create_model()
 
 print("Resuming model from {}".format(LOAD_MODEL_PATH))
 model.load_weights(LOAD_MODEL_PATH)
 
 print("Model loaded!")
+
+
 def resize_image(im, width, height, fill_color=(0, 0, 0)):
-    im = Image.fromarray(im.astype('uint8'))
-    new_im = ri.resize_contain(im, (width, height), bg_color=fill_color)
+    new_im = Image.fromarray(im.astype('uint8'))
+    new_im = ri.resize_contain(new_im, (width, height), bg_color=fill_color)
     return np.asarray(new_im)
+
 
 def onehot2rgb(onehot, color_dict):
     single_layer = np.argmax(onehot, axis=-1)
@@ -55,9 +59,11 @@ def rgb2onehot(rgb_arr):
         arr[:, :, i] = np.all(rgb_arr.reshape(
             (-1, 3)) == color_dict[i], axis=1).reshape(shape[:2])
     return arr
-    
+
+
 def test(x_test):
-    mask = np.zeros(shape=(IMAGE_INPUT_HEIGHT, IMAGE_INPUT_WIDTH, len(color_dict)))
+    mask = np.zeros(shape=(IMAGE_INPUT_HEIGHT,
+                           IMAGE_INPUT_WIDTH, len(color_dict)))
     for i in range(0, len(x_test)):
         cur_frame = x_test[i]
         cur_frame = resize_image(
@@ -67,11 +73,14 @@ def test(x_test):
 
         mask = model.predict_on_batch(cur_frame)
         mask = np.squeeze(mask, 0)
+        # mask = np.transpose(mask, (1, 0))
+        mask = np.reshape(mask, (IMAGE_INPUT_HEIGHT, IMAGE_INPUT_WIDTH, len(color_dict)))
         mask = onehot2rgb(mask, color_dict)
+
         ski.imsave("./TestOutput/segmentation" +
                    str(i)+".png", mask)
         mask = rgb2onehot(mask)
 
 
-x_test = pims.Video("./Training/original.mov")
+x_test = pims.Video("./Training/newtrainingdata.mov")
 test(x_test)

train.py
@@ -25,45 +25,55 @@ from data_generator import DataGenerator
 from model import SegmentationNetwork
 from clr import CyclicLR
 
-# Accommodate large batch size
-import sys
-sys.setrecursionlimit(10000)
+
+def onehot2rgb(onehot, color_dict):
+    single_layer = np.argmax(onehot, axis=-1)
+    output = np.zeros(onehot.shape[:2]+(3,))
+    for k in color_dict.keys():
+        output[single_layer == k] = color_dict[k]
+    return np.uint8(output)
+
 
 IMAGE_INPUT_WIDTH = 512
-IMAGE_INPUT_HEIGHT = 256
+IMAGE_INPUT_HEIGHT = 1024
 # Load a model from folder
 LOAD_MODEL = False
 LOAD_MODEL_PATH = "./Models/PlateSegmentation/weights-01.hdf5"
 color_dict = {0: (0,   0, 0),  # 0: Background
-              1: (255, 255, 255),  # 1: Red
-              #   2: (0, 0, 255),  # 2: Blue
+              1: (255, 0, 0),  # 1: Red
+              2: (0, 0, 255),  # 2: Blue
               }
 # Create data generator
-datagen = DataGenerator(x_train=pims.Video("./Training/fulloriginalcorrected.mov"),
-                        y_train=pims.Video("./Training/fullmaskcorrected-colored.mov"), batch_size=8,
-                        dim=(IMAGE_INPUT_HEIGHT, IMAGE_INPUT_WIDTH, 3), shuffle=True,
-                        color_dict=color_dict)
+datagen = DataGenerator(
+    x_train=pims.Video("./Training/newtrainingdata.mov"),
+    y_train=pims.Video("./Training/newtraininglabels.mov"), batch_size=4,
+    dim=(IMAGE_INPUT_HEIGHT, IMAGE_INPUT_WIDTH, 3), shuffle=True,
+    color_dict=color_dict)
+
 
 model = SegmentationNetwork(
-    layers=4, start_filters=12, squeeze_factor=8, num_classes=len(color_dict)).create_model()
+    layers=4, start_filters=12, squeeze_factor=12, num_classes=len(color_dict),
+    shape=(IMAGE_INPUT_HEIGHT, IMAGE_INPUT_WIDTH, 3)).create_model()
+
 if(LOAD_MODEL):
     print("Resuming model from {}".format(LOAD_MODEL_PATH))
     model.load_weights(LOAD_MODEL_PATH)
 
 sgd = optimizers.SGD(lr=0.0)
-model.compile(optimizer=sgd, loss="binary_crossentropy")
+model.compile(optimizer=sgd, loss="categorical_crossentropy",
+              metrics=['accuracy'])
 # model = UNet(input_size = (IMAGE_INPUT_HEIGHT, IMAGE_INPUT_WIDTH, 4)).unet()
 
 
-cyclical = CyclicLR(base_lr=0.00001, max_lr=0.15,
-                    step_size=123*2, mode="triangular2")
+cyclical = CyclicLR(base_lr=0.000001, max_lr=0.1,
+                    step_size=datagen.__len__() * 2, mode="triangular2")
 checkpoint = ModelCheckpoint(
     "./Models/PlateSegmentation/weights-{epoch:02d}.hdf5", verbose=1, period=1)
 
 
 def train(data_generator, callbacks):
     return model.fit_generator(generator=data_generator,
-                               callbacks=callbacks, epochs=4, verbose=1)
+                               callbacks=callbacks, epochs=100, verbose=1)
 
 
 def resize_image(im, width, height, fill_color=(0, 0, 0)):
@@ -80,13 +90,6 @@ def format_image_for_save(im, remove_batch=True):
         image = np.squeeze(im, -1)
     return image
 
-def onehot2rgb(onehot, color_dict):
-    single_layer = np.argmax(onehot, axis=-1)
-    output = np.zeros(onehot.shape[:2]+(3,))
-    for k in color_dict.keys():
-        output[single_layer == k] = color_dict[k]
-    return np.uint8(output)
-
 
 def rgb2onehot(rgb_arr):
     num_classes = len(color_dict)
@@ -99,7 +102,8 @@ def rgb2onehot(rgb_arr):
 
 
 def test(x_test):
-    mask = np.zeros(shape=(IMAGE_INPUT_HEIGHT, IMAGE_INPUT_WIDTH, len(color_dict)))
+    mask = np.zeros(shape=(IMAGE_INPUT_HEIGHT,
+                           IMAGE_INPUT_WIDTH, len(color_dict)))
     for i in range(0, len(x_test)):
         cur_frame = x_test[i]
         cur_frame = resize_image(
@@ -113,6 +117,7 @@ def test(x_test):
         ski.imsave("./TestOutput/segmentation" +
                    str(i)+".png", mask)
         mask = rgb2onehot(mask)
+
 
 history = train(data_generator=datagen, callbacks=[cyclical, checkpoint])
 # plt.plot(cyclical.history['lr'], cyclical.history['loss'])